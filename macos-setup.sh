43039eb1ef7909cc21443c7f97ffc18c2007877c
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2024-05-11 19:53:44 +0200

    Check if setup is running in a virtual machine

--- macos-setup.sh
+++ macos-setup.sh
@@ -2,7 +2,7 @@
 
 # Install xcode command line tools
 # Source: https://github.com/samdoran/ansible-collection-macos/blob/main/scripts/bootstrap.sh
-echo "Installing xcode command line tools"
+echo "Installing xcode command line tools."
 set -euo pipefail
 
 if [[ ! -d /Library/Developer/CommandLineTools ]]; then
@@ -14,30 +14,41 @@ if [[ ! -d /Library/Developer/CommandLineTools ]]; then
 fi
 
 # Clone the repository
-echo "Cloning macos-setup repository"
+echo "Cloning macos-setup repository."
 if [[ -d ${HOME}/macos-setup ]]; then
     rm -rf ${HOME}/macos-setup
 fi
 git clone https://github.com/daniel-mizsak/macos-setup.git ${HOME}/macos-setup --quiet
 
 # Create python virtual environment and install ansible
-echo "Creating python virtual environment and installing ansible"
+echo "Creating python virtual environment and installing ansible."
 if [[ ! -d ${HOME}/macos-setup/.venv ]]; then
     python3 -m venv --upgrade-deps ${HOME}/macos-setup/.venv > /dev/null
 fi
 source ${HOME}/macos-setup/.venv/bin/activate
 pip3 install -r ${HOME}/macos-setup/ansible/requirements.txt -qqq
 
+# Check if script is running on a virtual machine
+if system_profiler SPHardwareDataType | grep -q 'Model Identifier: .*Virtual';then
+    SKIP_TAGS="--skip-tags ignore-vm"
+    echo "Script is running on a virtual machine. Skipping tasks that are not needed."
+else
+    SKIP_TAGS=""
+    echo "Script is not running on a virtual machine."
+fi
+
+
 # Running setup scripts
 tasks_to_run=("homebrew" "package" "startup" "system" "finder" "dock" "dockutil" "dotfile")
 
 for task in "${tasks_to_run[@]}"
 do
-    echo "Running ${task} setup"
+    echo "Running ${task} setup."
 
     ansible-playbook \
     ${HOME}/macos-setup/ansible/playbook.yml \
-    --extra-vars "task_name=${task}"
+    --extra-vars "task_name=${task}" \
+    ${SKIP_TAGS} \
 
     if [ "$task" == "homebrew" ]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -45,6 +56,6 @@ do
 done
 
 # Restart finder and dock
-echo "Restarting Finder and Dock"
+echo "Restarting Finder and Dock."
 killall Finder
 killall Dock
