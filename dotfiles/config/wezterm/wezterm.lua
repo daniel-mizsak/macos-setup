93eaa7c33f3dad61cc362e08a42d0f849875b3ee
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2024-07-28 21:05:53 +0200

    Small config updates

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -7,16 +7,17 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = "catppuccin-mocha"
-
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14
 
-config.enable_tab_bar = false
+config.initial_cols = 100
+config.initial_rows = 32
 
--- config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
+config.enable_tab_bar = false
+config.macos_window_background_blur = 20
+config.window_background_opacity = 0.95
 config.window_close_confirmation = "NeverPrompt"
+-- config.window_decorations = "RESIZE"
 
 config.keys = {
     -- Navigation
