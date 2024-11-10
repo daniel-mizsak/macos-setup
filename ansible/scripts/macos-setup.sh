44c0082ea11bde1d6ac5d3471652126bac39bb2d
Daniel Mizsak <62774880+daniel-mizsak@users.noreply.github.com>
2024-11-10 12:42:20 +0100

    Remove terminal setup from programs ansible tasks

--- ansible/scripts/macos-setup.sh
+++ ansible/scripts/macos-setup.sh
@@ -5,7 +5,7 @@ set -e # exit if a command fails
 curl -fsSL https://raw.githubusercontent.com/daniel-mizsak/macos-setup/main/ansible/scripts/prepare-environment.sh -o /tmp/prepare-environment.sh
 source /tmp/prepare-environment.sh
 
-tasks="package" "system" "finder" "dock" "dockutil" "dotfile"
+tasks="programs" "system" "dockutil" "terminal"
 source ${HOME}/macos-setup/ansible/scripts/run-tasks.sh $tasks
 
 # Restart finder and dock
